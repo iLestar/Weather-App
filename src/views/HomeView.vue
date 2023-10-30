663cf5e0603322b767e801d459251a17b4b5c5ad
iLestar <juanlestar0408@gmail.com>
2023-10-30 18:17:25 -0500

    Redirect Route + change API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,29 +3,45 @@
     <div class="pt-4 mb-8 relative">
       <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="Search for a city or state"
         class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
-      <ul class="absolute bg-weather-secondary text-white shadow-md py-2 px-1 top-[66px] w-full" v-if="apiSearchResult || (apiSearchResult === null && searchError === true)">
-        <p class="py-2" v-if="searchError === true">Sorry, no result matches, try a different term.</p>
-        <template v-else>
-          <li class="py-2 cursor-pointer">
-            {{ apiSearchResult.name }}, {{ apiSearchResult.sys.country }}
-          </li>
-        </template>
-      </ul>
+        <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]" v-if="apiSearchResult">
+          <p class="py-2" v-if="searchError">Sorry, something went wrong, please try again.</p>
+          <p class="py-2" v-if="!searchError && apiSearchResult.length === 0">No results match your query, try a different term.</p>
+          <template v-else>
+            <li v-for="searchResult in apiSearchResult" :key="searchResult.id" class="py-2 cursor-pointer" @click="previewCity(searchResult)">
+              {{ searchResult.place_name }}
+            </li>
+          </template>
+        </ul>
     </div>
   </main>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 import axios from 'axios'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const previewCity = (searchResult) => {
+  console.log(searchResult);
+  const [city, state] = searchResult.place_name.split(",")
+  router.push({
+    name: 'cityView',
+    params: { state: state.replaceAll(" ", ""), city: city },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true
+    }
+  })  
+}
 
 const searchQuery = ref("")
 const queryTimeout = ref("null")
 const apiSearchResult = ref(null)
 const searchError = ref(null)
 
-const api_key = 'e67840bae93839e21012aedf830ab540'
-const url_base = 'https://api.openweathermap.org/data/2.5/'
+const APIKey = 'pk.eyJ1IjoiaWxlc3RhciIsImEiOiJjbG9kaG4wNDQwN2x3MmtzMTZuaTU1N2JiIn0.YmLzJqO4sAJQZZIMDFyzSw'
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value)
@@ -33,8 +49,8 @@ const getSearchResults = () => {
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
       try {
-        const result = await axios.get(`${url_base}weather?q=${searchQuery.value}&units=metric&lang=es&appid=${api_key}`)
-        apiSearchResult.value = result.data
+        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${APIKey}&types=place`)
+        apiSearchResult.value = result.data.features
       } catch (error) {
         searchError.value = true
       }
